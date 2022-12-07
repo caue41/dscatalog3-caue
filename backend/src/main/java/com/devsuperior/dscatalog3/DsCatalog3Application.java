2075d4d76190f7772c2b51cdb57074e537dd15a2
Cauê Gonçalves Santana <caue41@gmail.com>
2022-12-07 17:31:28 -0300

    Create project

---
+++ backend/src/main/java/com/devsuperior/dscatalog3/DsCatalog3Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalog3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalog3Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalog3/DsCatalog3ApplicationTests.java
@@ -0,0 +1,13 @@
+/*package com.devsuperior.dscatalog3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalog3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}*/
