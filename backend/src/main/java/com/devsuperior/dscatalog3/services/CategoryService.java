f1692cf01402df746bafec2a762beed93da8e583
Cauê Gonçalves Santana <caue41@gmail.com>
2022-12-08 19:14:31 -0300

    CategoryDTO

--- backend/src/main/java/com/devsuperior/dscatalog3/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog3/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dscatalog3.services;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog3.dto.CategoryDTO;
 import com.devsuperior.dscatalog3.entities.Category;
 import com.devsuperior.dscatalog3.repositories.CategoryRepository;
 
@@ -16,7 +18,12 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for(Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		return listDto;
 	}
 }
