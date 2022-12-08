f9faf568febef912c16f58c1a77f4e329adfee47
Cauê Gonçalves Santana <caue41@gmail.com>
2022-12-08 17:40:22 -0300

    category resource, category service and h2 database

--- backend/src/test/java/com/devsuperior/dscatalog3/DsCatalog3ApplicationTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog3/DsCatalog3ApplicationTests.java
@@ -1,4 +1,4 @@
-/*package com.devsuperior.dscatalog3;
+package com.devsuperior.dscatalog3;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,4 +10,4 @@ class DsCatalog3ApplicationTests {
 	void contextLoads() {
 	}
 
-}*/
+}
